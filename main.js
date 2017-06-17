004f58a5aa766c9ddd3f37d4abd5b986f2637a36
Garmhausen <robertgarmhausen@gmail.com>
2017-06-16 21:36:06 -0400

    Add search() function for processing search string.

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 let str = "";
 let input = document.getElementById('')
+
+
+function search(str) {
+  // receives search phrase string, returns json data.
+  let api = 'https://api.soundcloud.com/users/?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f&' + str;
+  fetch(api)
+    .then(
+      function(response) {
+        if (response.status !== 200) {
+          console.log('Problem.  Code: ' + response.status);
+          return;
+        }
+
+        response.json().then(function(data) {
+          console.log(data);
+          return data;
+        })
+      }
+    )
+}
